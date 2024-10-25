56121cea5c7cd79fe2d047c65f97a2239a5dad87
7vTr0x7 <7vTrox7@gmail.com>
2024-10-25 16:12:02 +0530

    feat: routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Home from "./pages/Home/Home";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./pages/Search/Search";
+
 const App = () => {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </Router>
     </>
   );
 };
