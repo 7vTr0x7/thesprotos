5d7cd9a457e63ad91962c3eca3cb99980c807322
7vTr0x7 <7vTrox7@gmail.com>
2024-10-25 18:09:31 +0530

    feat: Search

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import SearchNav from "./features/SearchNav";
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
+import data from "../../utils/players.json";
+import Player from "./features/player";
 
 const Search = () => {
   return (
-    <div>
+    <>
       <SearchNav />
-    </div>
+      <div className="bg-black h-[100vh] px-44 py-12">
+        <div className="flex justify-between items-center text-gray-50">
+          <p className="text-2xl font-semibold text-gray-50">Players</p>
+          <HiOutlineArrowLongRight className="text-2xl " />
+        </div>
+        <div className="mt-5 flex flex-wrap gap-5">
+          {data?.players.map((player, index) => (
+            <Player player={player} key={index} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ frontend/src/pages/Search/features/Player.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Player = ({ player }) => {
+  return (
+    <div className="bg=[#151515] py-2 px-4">
+      <p>{player}</p>
+    </div>
+  );
+};
+
+export default Player;
