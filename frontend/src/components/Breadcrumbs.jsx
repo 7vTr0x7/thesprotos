562552f9a74f465a8a4e3d1bfae6c1b5d46e870e
7vTr0x7 <7vTrox7@gmail.com>
2024-10-28 11:51:53 +0530

    feat: Breadcrumbs

---
+++ frontend/src/components/Breadcrumbs.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Breadcrumbs = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
+  let breadcrumbPath = "";
+
+  return (
+    <div className="text-gray-50">
+      {pathnames.length > 0 && <Link to="/">Home</Link>}
+
+      {pathnames &&
+        pathnames.map((path, index) => {
+          breadcrumbPath += `/${path}`;
+
+          const isLast = index === pathnames.length - 1;
+          return isLast ? (
+            <span key={breadcrumbPath}>
+              {" "}
+              /{" "}
+              <span className="font-semibold">
+                {`${path.charAt(0).toUpperCase()}${path.slice(1)}`}
+              </span>
+            </span>
+          ) : (
+            <span key={breadcrumbPath}>
+              <Link to={breadcrumbPath}>{path}</Link>
+            </span>
+          );
+        })}
+    </div>
+  );
+};
+
+export default Breadcrumbs;
--- frontend/src/pages/Fixtures/Fixtures.jsx
+++ frontend/src/pages/Fixtures/Fixtures.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Breadcrumbs from "../../components/Breadcrumbs";
 
 const Fixtures = () => {
   return (
@@ -9,6 +10,9 @@ const Fixtures = () => {
       <div>
         <div className="bg-gradient-to-r from-yellow-200 to-yellow-300 text-center text-brown-700 font-semibold py-4 h-36 flex justify-center items-center text-2xl">
           Fixtures
+        </div>
+        <div className="px-14 py-3 bg-black">
+          <Breadcrumbs />
         </div>
       </div>
       <Footer />
