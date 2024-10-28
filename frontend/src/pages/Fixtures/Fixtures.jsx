ba5b76dde13402396c58c456af7754ed3b126b36
7vTr0x7 <7vTrox7@gmail.com>
2024-10-28 11:27:54 +0530

    page: Fixtures

--- frontend/src/pages/Fixtures/Fixtures.jsx
+++ frontend/src/pages/Fixtures/Fixtures.jsx
@@ -6,7 +6,11 @@ const Fixtures = () => {
   return (
     <>
       <Header />
-      <div className="py-10"></div>
+      <div>
+        <div className="bg-gradient-to-r from-yellow-200 to-yellow-300 text-center text-brown-700 font-semibold py-4 h-36 flex justify-center items-center text-2xl">
+          Fixtures
+        </div>
+      </div>
       <Footer />
     </>
   );
