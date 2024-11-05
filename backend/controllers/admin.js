837a67d159da252984ef89d6aca8b46599ad89b8
7vTr0x7 <7vTrox7@gmail.com>
2024-11-05 15:03:09 +0530

    feat: admin controllers - addUpcomingMatch

---
+++ backend/controllers/admin.js
@@ -0,0 +1,20 @@
+import { UpcomingMatch } from "../models/UpcomingMatch.model";
+
+export const addUpcomingMatch = async (req, res) => {
+  try {
+    const upcomingMatch = await UpcomingMatch.create(req.body);
+    if (upcomingMatch) {
+      res.json({
+        success: true,
+        upcomingMatch,
+      });
+    } else {
+      res.status(404).json({
+        success: true,
+        message: "Upcoming match not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json(`Failed to add admin`);
+  }
+};
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { addUpcomingMatch } from "../controllers/admin";
 
 const router = express.Router();
+
+router.post("/add/upcoming-match", addUpcomingMatch);
 
 export default router;
