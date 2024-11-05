349554638777fc904a8c2bb05d90a3ac62aa4905
7vTr0x7 <7vTrox7@gmail.com>
2024-11-05 13:30:23 +0530

    feat: server setup

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,20 @@
+import express from "express";
 import { initializeDatabase } from "./db/db.connection.js";
+import cors from "cors";
+
+const app = express();
+app.use(express.json);
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 initializeDatabase();
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
