4156178b5be3af766a550be7ad39ca3de7c69923
7vTr0x7 <7vTrox7@gmail.com>
2024-11-05 16:38:52 +0530

    feat: isAuthenticated

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,14 +2,21 @@ import express from "express";
 import {
   addMultipleUpcomingMatches,
   addUpcomingMatch,
+  adminLogin,
   registerAdmin,
 } from "../controllers/admin.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/add/upcoming-match", addUpcomingMatch);
-router.post("/add/multiple/upcoming-matches", addMultipleUpcomingMatches);
-
 router.post("/register", registerAdmin);
+router.post("/login", adminLogin);
+
+router.post("/add/upcoming-match", isAuthenticated, addUpcomingMatch);
+router.post(
+  "/add/multiple/upcoming-matches",
+  isAuthenticated,
+  addMultipleUpcomingMatches
+);
 
 export default router;
