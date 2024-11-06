3b98baf591af84c60497e2dd4fe6c1af69dccc2c
7vTr0x7 <7vTrox7@gmail.com>
2024-11-06 17:18:56 +0530

    feat: getSponsorData

--- frontend/src/pages/Home/features/Grid/features/Sponsor/Sponsor.jsx
+++ frontend/src/pages/Home/features/Grid/features/Sponsor/Sponsor.jsx
@@ -1,14 +1,48 @@
-import React from "react";
-import { sponsor } from "../../../../../../images/index.js";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Sponsor = ({ text }) => {
+  const [sponsor, setSponsor] = useState("");
+
+  const getSponsorData = async () => {
+    try {
+      const res = await fetch("http://localhost:4000/api/user/sponsor", {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (!res.ok) {
+        console.log("Failed to get data");
+      }
+
+      const data = await res.json();
+
+      if (data.success) {
+        setSponsor(data?.sponsor[0]?.imageUrl);
+        console.log(data);
+      }
+    } catch (error) {
+      console.log("failed to get UpcomingMatch Data", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getSponsorData();
+  }, []);
+
   return (
-    <div className="text-gray-50 mt-7 mb-3">
-      <p className={`text-[28px] mb-4  ${text && "mt-6"}`}>Sponsor</p>
-      <div className="my-3 ">
-        <img alt="Sponsor" src={sponsor} className="w-full h-auto shadow-lg" />
+    sponsor && (
+      <div className="text-gray-50 mt-7 mb-3">
+        <p className={`text-[28px] mb-4  ${text && "mt-6"}`}>Sponsor</p>
+        <div className="my-3 ">
+          <img
+            alt="Sponsor"
+            src={sponsor}
+            className="w-full h-auto rounded-lg shadow-lg"
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
