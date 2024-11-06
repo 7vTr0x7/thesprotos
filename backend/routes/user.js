98afdba767f4f80de1b725931722db47a6eeecb7
7vTr0x7 <7vTrox7@gmail.com>
2024-11-06 16:53:48 +0530

    feat: getSponsor

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { getBanner, getUpcomingMatches } from "../controllers/user.js";
+import {
+  getBanner,
+  getSponsor,
+  getUpcomingMatches,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/upcoming-matches", getUpcomingMatches);
 router.get("/banner", getBanner);
+router.get("/sponsor", getSponsor);
 
 export default router;
