4e29e30be468f38d7cf858b154bf5d9b998f4530
7vTr0x7 <7vTrox7@gmail.com>
2024-11-06 17:26:57 +0530

    feat: StarPerformers

---
+++ backend/models/StarPerformers.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const StarPerformersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  achievement: {
+    type: String,
+    required: true,
+  },
+  tournament: {
+    type: String,
+    required: true,
+  },
+  goals: {
+    type: Number,
+    required: true,
+  },
+  assists: {
+    type: Number,
+    required: true,
+  },
+  matches_played: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const StarPerformers = mongoose.model(
+  "StarPerformers",
+  StarPerformersSchema
+);
