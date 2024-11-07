084a112e66d0a070ea4be2a30184d8f8d04dc32b
7vTr0x7 <7vTrox7@gmail.com>
2024-11-07 11:24:51 +0530

    feat: FeaturedPlayer.model

---
+++ backend/models/FeaturedPlayer.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const FeaturedPlayerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  goals: {
+    type: String,
+    required: true,
+  },
+  assists: {
+    type: String,
+    required: true,
+  },
+  rank: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+});
+
+export const FeaturedPlayer = mongoose.model(
+  "FeaturedPlayer",
+  FeaturedPlayerSchema
+);
