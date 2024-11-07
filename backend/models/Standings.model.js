2d45cdbe1b030ff15178caf82cc237536c7b5616
7vTr0x7 <7vTrox7@gmail.com>
2024-11-07 17:34:51 +0530

    fix:  Standings.model

---
+++ backend/models/Standings.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const StandingsSchema = new mongoose.Schema({
+  club: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: Number,
+    required: true,
+  },
+  played: {
+    type: String,
+    required: true,
+  },
+  won: {
+    type: Number,
+    required: true,
+  },
+  drawn: {
+    type: Number,
+    required: true,
+  },
+  lost: {
+    type: Number,
+    required: true,
+  },
+  goals: {
+    type: String,
+    required: true,
+  },
+  last5: {
+    type: [String],
+    required: true,
+  },
+
+  points: {
+    type: Number,
+    required: true,
+  },
+});
+
+export const Standings = mongoose.model("Standings", StandingsSchema);
