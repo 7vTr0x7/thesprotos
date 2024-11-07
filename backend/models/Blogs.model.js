8c7c989078cf548590fd355b4eba4e69e3261a54
7vTr0x7 <7vTrox7@gmail.com>
2024-11-07 14:55:38 +0530

    feat: Blogs.model

---
+++ backend/models/Blogs.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const BlogSchema = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String,
+    required: true,
+  },
+  by: {
+    type: String,
+    required: true,
+  },
+});
+
+export const Blog = mongoose.model("Blog", BlogSchema);
