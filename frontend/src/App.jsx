f447d7f0af8d677e47e9a253133751bda439772c
7vTr0x7 <7vTrox7@gmail.com>
2024-11-07 15:15:38 +0530

    fix: fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ContactUs from "./pages/ContactUs/ContactUs";
 import Fixtures from "./pages/Fixtures/Fixtures";
@@ -10,10 +10,55 @@ import PlayerDetails from "./pages/PlayerDetails/PlayerDetails";
 import Players from "./pages/Players/Players";
 import ShimmerUI from "./pages/ShimmerUI/ShimmerUI";
 import NewsDetails from "./pages/News/features/NewsDetails.jsx/NewsDetails";
+import { setNextMatch } from "./app/slices/nextMatchSlice";
+import { setLiveMatch } from "./app/slices/liveMatchSlice";
+import { setUpcomingMatch } from "./app/slices/upcomingMatchesSlice";
+import { useDispatch } from "react-redux";
 const Home = lazy(() => import("./pages/Home/Home"));
 const Search = lazy(() => import("./pages/Search/Search"));
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const getMatchesData = async () => {
+    try {
+      const res = await fetch(
+        "http://localhost:4000/api/user/upcoming-matches",
+        {
+          method: "GET",
+          credentials: "include",
+        }
+      );
+
+      if (!res.ok) {
+        console.log("Failed to get data");
+      }
+
+      const data = await res.json();
+
+      if (data.success) {
+        dispatch(setUpcomingMatch(data.upcomingMatches));
+        const liveMatch = data.upcomingMatches.find(
+          (match) => match.status === "Live"
+        );
+
+        dispatch(setLiveMatch(liveMatch));
+
+        const nextMatch = data.upcomingMatches.find(
+          (match) => match.status === "Upcoming"
+        );
+
+        dispatch(setNextMatch(nextMatch));
+      }
+    } catch (error) {
+      console.log("failed to get UpcomingMatch Data", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getMatchesData();
+  }, []);
+
   return (
     <div>
       <Router>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,56 +1,11 @@
-import React, { useEffect, useState } from "react";
-import Header from "../../components/Header";
-import UpcomingMatches from "./features/UpcomingMatches/UpcomingMatches";
+import React from "react";
 import Footer from "../../components/Footer";
+import Header from "../../components/Header";
 import Grid from "./features/Grid/Grid";
 import News from "./features/News/News";
-import { setNextMatch } from "../../app/slices/nextMatchSlice";
-import { setLiveMatch } from "../../app/slices/liveMatchSlice";
-import { useDispatch } from "react-redux";
-import { setUpcomingMatch } from "../../app/slices/upcomingMatchesSlice";
+import UpcomingMatches from "./features/UpcomingMatches/UpcomingMatches";
 
 const Home = () => {
-  const dispatch = useDispatch();
-
-  const getMatchesData = async () => {
-    try {
-      const res = await fetch(
-        "http://localhost:4000/api/user/upcoming-matches",
-        {
-          method: "GET",
-          credentials: "include",
-        }
-      );
-
-      if (!res.ok) {
-        console.log("Failed to get data");
-      }
-
-      const data = await res.json();
-
-      if (data.success) {
-        dispatch(setUpcomingMatch(data.upcomingMatches));
-        const liveMatch = data.upcomingMatches.find(
-          (match) => match.status === "Live"
-        );
-
-        dispatch(setLiveMatch(liveMatch));
-
-        const nextMatch = data.upcomingMatches.find(
-          (match) => match.status === "Upcoming"
-        );
-
-        dispatch(setNextMatch(nextMatch));
-      }
-    } catch (error) {
-      console.log("failed to get UpcomingMatch Data", error.message);
-    }
-  };
-
-  useEffect(() => {
-    getMatchesData();
-  }, []);
-
   return (
     <div className="select-none">
       <Header />
